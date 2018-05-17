45868714559036031c969ce28d555b409dbed3ba
Laia Sanahuja <laia.sase@gmail.com>
2018-05-17 11:31:08 +0200

    “Versió 0.4 de l'examen documentar les classes”.

--- src/examenm05/PantryTester.java
+++ src/examenm05/PantryTester.java
@@ -3,11 +3,17 @@ package examenm05;
 import java.util.Scanner;
 
 /**
- *
+ * Classe que executa el programa per untar marmelada 
  * @author Admin
  */
 public class PantryTester{
-	public static void main(String[] args) {
+
+    /**
+     * metode main executa els metodes de les altres classes per tal de que en 
+     * poguem untar la marmelada que vulguem
+     * @param args
+     */
+    public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int num, amount;
 		Jam goose = new Jam("Gooseberry", "7/4/2015", 12);
@@ -19,6 +25,10 @@ public class PantryTester{
 		System.out.println("Welcome to Mother Hubbard's Party!");
 		System.out.println("The jams are: ");
 		hubbard.print();
+                /**
+                 * Bucle que et demana quina marmelada vols i la quantitat que en necessites
+                 * per sortir del bucle hem de posar un index erroni i despres -1
+                 */
 		do {
 			System.out.println("Enter your selection (1,2,or 3):");
 			num = scan.nextInt();
