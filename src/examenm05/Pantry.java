032092f546764572300f258fbecc149ee9499bee
Laia Sanahuja <laia.sase@gmail.com>
2018-05-17 11:34:24 +0200

    “Versió 0.4 de l'examen documentar les classes”.

--- src/examenm05/Pantry.java
+++ src/examenm05/Pantry.java
@@ -6,42 +6,64 @@
 package examenm05;
 
 /**
+ * Classe Pantry
  *
  * @author Admin
  */
 public class Pantry {
-	private Jam jar1;
-	private Jam jar2;
-	private Jam jar3;
-	private Jam selected;
 
-	Pantry(Jam jar1, Jam jar2, Jam jar3) {
-		this.jar1 = jar1;
-		this.jar2 = jar2;
-		this.jar3 = jar3;
-		selected = null;
-	}
+    /*Marmelada 1*/
+    private Jam jar1;
+    /*Marmelada 2*/
+    private Jam jar2;
+    /*Marmelada 3*/
+    private Jam jar3;
+    /*Marmelada seleccionada*/
+    private Jam selected;
+    /**
+     * constructor de l'objecte pantry
+     * @param jar1 marmelada 1
+     * @param jar2 mermalada 2
+     * @param jar3 mermalada 3
+     */
+    Pantry(Jam jar1, Jam jar2, Jam jar3) {
+        this.jar1 = jar1;
+        this.jar2 = jar2;
+        this.jar3 = jar3;
+        selected = null;
+    }
 
-	public void print() {
-		System.out.print("1: ");
-		jar1.print();
-		System.out.print("2: ");
-		jar2.print();
-		System.out.print("3: ");
-		jar3.print();
-	}
+    /**
+     * excriu les dades de pantry
+     */
+    public void print() {
+        System.out.print("1: ");
+        jar1.print();
+        System.out.print("2: ");
+        jar2.print();
+        System.out.print("3: ");
+        jar3.print();
+    }
 
-	public void select(int jarNumber) {
-		if (jarNumber == 1) {
-			selected = jar1;
-		} else if (jarNumber == 2) {
-			selected = jar2;
-		} else {
-			selected = jar3;
-		}
-	}
+    /**
+     * selecciona una marmelada
+     * @param jarNumber numero de la mermelada que volem seleccionar
+     */
+    public void select(int jarNumber) {
+        if (jarNumber == 1) {
+            selected = jar1;
+        } else if (jarNumber == 2) {
+            selected = jar2;
+        } else {
+            selected = jar3;
+        }
+    }
 
-	public void spread(int oz) {
-		selected.spread(oz);
-	}
+    /**
+     * metode per untar marmelada
+     * @param oz quantitat de mermelada que necessitem
+     */
+    public void spread(int oz) {
+        selected.spread(oz);
+    }
 }
